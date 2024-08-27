c81209ec4d82dd2f54ea8ea38290342d71bc1b59
Independent Creator <independentcreator@protonmail.com>
2024-08-27 16:23:58 +0100

    Working deploy-wrangler.sh

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,44 +5,60 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-import compress from 'astro-compress';
 import icon from 'astro-icon';
 import tasks from './src/utils/tasks';
 import { readingTimeRemarkPlugin } from './src/utils/frontmatter.mjs';
 import { SITE } from './src/utils/config.ts';
 import cloudflare from '@astrojs/cloudflare';
+import compress from 'astro-compress';
+
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const whenExternalScripts = (items = []) => false ? Array.isArray(items) ? items.map(item => item()) : [items()] : [];
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE.site,
   base: SITE.base,
   trailingSlash: SITE.trailingSlash ? 'always' : 'ignore',
   output: 'server',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), mdx(), icon({
-    include: {
-      tabler: ['*'],
-      'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
-    }
-  }), ...whenExternalScripts(() => partytown({
-    config: {
-      forward: ['dataLayer.push']
-    }
-  })), tasks(), compress({
+  adapter: cloudflare({
+    mode: 'directory',
+    runtime: {
+      mode: 'local',
+      persistTo: './persisted',
+    },
+    assets: 'preset',
+  }),
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    compress({
     CSS: true,
     HTML: {
       removeAttributeQuotes: false
-    },
+     },
     Image: false,
     JavaScript: true,
     SVG: true,
     Logger: 1
-  })],
+    }),
+    sitemap(),
+    mdx(),
+    icon({
+      include: {
+        tabler: ['*'],
+        'flat-color-icons': ['template', 'gallery', 'approval', 'document', 'advertising', 'currency-exchange', 'voice-presentation', 'business-contact', 'database']
+      }
+    }),
+    ...whenExternalScripts(() => partytown({
+      config: {
+        forward: ['dataLayer.push']
+      }
+    })),
+    tasks(),
+  ],
   markdown: {
-    /*syntaxHighlight: 'prism',*/
     syntaxHighlight: false
   },
   vite: {
@@ -50,9 +66,9 @@ export default defineConfig({
       alias: {
         '~': path.resolve(__dirname, './src')
       }
-    }
+    },
+    build: {
+      assetsInlineLimit: 0,
+    },
   },
-  adapter: cloudflare({
-    mode: 'directory'
-  })
 });
---
+++ deploy-wrangler.sh
@@ -0,0 +1,6 @@
+# Login to Cloudflare account from CLI
+wrangler login
+# Run your build command
+pnpm run build
+# Create new deployment
+pnpm wrangler pages deploy dist
